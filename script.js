742ee18fba23ad83e2824bb48b1bc11bd6ea5ca8
leilei-anthony <lesteranthonyjr@gmail.com>
2025-03-12 16:05:54 +0800

    Add playRound function

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-let humanScore = 0
-let computerScore = 0
-
 function getHumanChoice() {
-    return prompt("Rock, Paper, or Scissors?")
+    let choice = prompt("Rock, Paper, or Scissors?")
+    return choice.toLowerCase()
 }
 
 function getComputerChoice() {
@@ -20,3 +18,58 @@ function getComputerChoice() {
     }
 }
 
+function playRound(humanChoice, computerChoice) {
+    let text = ""
+    switch(humanChoice) {
+        case "rock":
+            switch(computerChoice) {
+                case "rock":
+                    text = "It's a tie!"
+                    break
+                case "paper":
+                    text = "You lose! Paper beats Rock."
+                    break
+                case "scissors":
+                    text = "You win! Rock beats Scissors."
+                    break
+            }
+            break
+        case "paper":
+            switch(computerChoice) {
+                case "rock":
+                    text = "You win! Paper beats Rock."
+                    break
+                case "paper":
+                    text = "It's a tie!"
+                    break
+                case "scissors":
+                    text = "You lose! Scissors beats Paper."
+                    break
+            }
+            break
+        case "scissors":
+            switch(computerChoice) {
+                case "rock":
+                    text = "You lose! Rock beats Scissors."
+                    break
+                case "paper":
+                    text = "You win! Scissors beats Paper."
+                    break
+                case "scissors":
+                    text = "It's a tie!"
+                    break
+            }
+            break
+    }
+
+    console.log(text)
+}
+
+function playGame() {
+    let humanScore = 0
+    let computerScore = 0
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection)
